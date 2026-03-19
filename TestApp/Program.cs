10a77e3c56c3956c59cdebfe10154b17c0e1ceaa
Edigaryan Inga <edigaryaninga@gmail.com>
2026-03-19 18:46:31 +0400

    Fixed project structure and added Stack/Queue tests

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,20 +1,25 @@
-﻿using MyStackProj;
+﻿using System;
+using MyStackProj;
+using MyQueueProj; 
 
 class Program
 {
-    public void Main()
+    static void Main()
     {
         MyStack<int> stack = new MyStack<int>();
-
         stack.Push(10);
         stack.Push(20);
         stack.Push(30);
 
-        Console.WriteLine( stack.Peek());
+        Console.WriteLine(stack.Peek());
         Console.WriteLine(stack.Pop());
         Console.WriteLine(stack.Peek());
 
-        Console.WriteLine();
-        Console.ReadKey();
+        Console.WriteLine("--- Queue Test ---");
+
+        MyQueue<int> queue = new MyQueue<int>();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        Console.WriteLine(queue.Dequeue());
     }
 }
